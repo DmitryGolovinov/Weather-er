be3a6ada71175462e5bf86da150aafbd469051cc
Coverkincel <coverkincel@gmail.com>
2019-06-17 15:52:10 +0300

    Updates

---
+++ precache-manifest.e1542835e6364d9c177d7030a04a6a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba655174f81f51e95c3546b78048656a",
+    "url": "/Weather-er/index.html"
+  },
+  {
+    "revision": "81da8d936a4f1e42c012",
+    "url": "/Weather-er/static/css/main.b9f6119e.chunk.css"
+  },
+  {
+    "revision": "bc87192685f354485dda",
+    "url": "/Weather-er/static/js/2.fe1f692c.chunk.js"
+  },
+  {
+    "revision": "81da8d936a4f1e42c012",
+    "url": "/Weather-er/static/js/main.02115fec.chunk.js"
+  },
+  {
+    "revision": "4fdf5be625acb68c0ecf",
+    "url": "/Weather-er/static/js/runtime~main.822379cf.js"
+  },
+  {
+    "revision": "73adc9ffaf75383691c0ebbaf4019958",
+    "url": "/Weather-er/static/media/background-bottom.73adc9ff.png"
+  }
+]);
